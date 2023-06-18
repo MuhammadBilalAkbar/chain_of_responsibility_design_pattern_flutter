8b5bd3ade75f04d1d1f333262f1f73e6afeae9b1
MuhammadBilalAkbar <mbilalakbarkh786@gmail.com>
2023-06-18 17:14:53 +0500

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,26 +10,28 @@ void main() {
   consoleLogger.next = emailLogger;
   emailLogger.next = fileLogger;
 
-  // consoleLogger.log(LogLevel.debug, "Some amazingly helpful debug message");
-  // consoleLogger.log(LogLevel.info, "Pretty important information");
-  //
-  // consoleLogger.log(LogLevel.warning, "This is a warning!");
-  // consoleLogger.log(LogLevel.error, "This shows an error!");
-  //
-  consoleLogger.log(LogLevel.functionalError, "This is not a show stopper");
-  consoleLogger.log(LogLevel.functionalMessage, "This is basically just info");
+  consoleLogger.log(LogLevel.debug, "Some amazingly helpful debug message");
+  // Output on console:
+  // [ConsoleLogger]: Some amazingly helpful debug message
+  consoleLogger.log(LogLevel.info, "Pretty important information");
+  // Output on console:
+  // [ConsoleLogger]: Pretty important information
 
-  /// Output on console:
-  /*
-    [ConsoleLogger]: Some amazingly helpful debug message
-    [ConsoleLogger]: Pretty important information
-    [ConsoleLogger]: This is a warning!
-    [FileLogger]: This is a warning!
-    [ConsoleLogger]: AHHHHHHHHH!
-    [FileLogger]: AHHHHHHHHH!
-    [ConsoleLogger]: This is not a show stopper
-    [EmailLogger]: This is not a show stopper
-    [ConsoleLogger]: This is basically just info
-    [EmailLogger]: This is basically just info
-  */
+  consoleLogger.log(LogLevel.warning, "This is a warning!");
+  // Output on console:
+  // [ConsoleLogger]: This is a warning!
+  // [FileLogger]: This is a warning!
+  consoleLogger.log(LogLevel.error, "This shows an error!");
+  // Output on console:
+  // [ConsoleLogger]: This shows an error!
+  // [FileLogger]: This shows an error!
+
+  consoleLogger.log(LogLevel.functionalError, "This is not a show stopper");
+  // Output on console:
+  // [ConsoleLogger]: This is not a show stopper
+  // [EmailLogger]: This is not a show stopper
+  consoleLogger.log(LogLevel.functionalMessage, "This is basically just info");
+  // Output on console:
+  // [ConsoleLogger]: This is basically just info
+  // [EmailLogger]: This is basically just info
 }
